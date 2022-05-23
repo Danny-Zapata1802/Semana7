8f4c15ba9ad9420296b897f22b11fbcb3cf550ac
DANNY-ZAP <36015978+Danny-AZP@users.noreply.github.com>
2022-05-22 22:01:10 -0500

    Agregar archivos de proyecto.

---
+++ Semana7/ConsultaRegistro.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Semana7.Models;
+using SQLite;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConsultaRegistro : ContentPage
+    {
+        private SQLiteAsyncConnection con;
+        private ObservableCollection<Estudiante> tablaEstudiantes;
+        public ConsultaRegistro()
+        {
+            InitializeComponent();
+            con = DependencyService.Get<Database>().GetConnection();
+            get();
+        }
+        public async void get()
+        {
+            try
+            {
+                var Resultado = await con.Table<Estudiante>().ToListAsync();
+                tablaEstudiantes = new ObservableCollection<Estudiante>(Resultado);
+                ListaUsuario.ItemsSource = tablaEstudiantes;
+                base.OnAppearing();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void ListaUsuario_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var obj = (Estudiante)e.SelectedItem;
+            var item = obj.Id.ToString();
+            var ID = Convert.ToInt32(item);
+            var nombreItem = obj.Nombre;
+            string nombre = nombreItem.ToString();
+            var usuarioItem = obj.Usuario;
+            string usuario = usuarioItem.ToString();
+            var passwordItem = obj.Password;
+            string password = passwordItem.ToString();
+            try
+            {
+                Navigation.PushAsync(new Elemento(ID, nombre, usuario, password));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Semana7/Database.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana7
+{
+    public interface Database
+    {
+        SQLiteAsyncConnection GetConnection(); // Implementar en cada proyecto
+    }
+}
